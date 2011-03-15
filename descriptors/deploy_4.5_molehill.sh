851149f3d22763b6587512db148bf91f0b34a354
Conrad Winchester <conrad@mindless.com>
2011-03-15 07:29:31 +0000

    Got molehill deploying to a snapshot at flasjnexus.org, can build but get a missing Context3D error when trying to run

---
+++ descriptors/deploy_4.5_molehill.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export MAVEN_OPTS=-Xmx1024m
+zip flex_sdk_4.5.0.19786.zip empty.swc
+zip flex_sdk_4.5.0.19786.zip frameworks/libs/flashplayer_inc_playerglobal_022711.swc
+zip flex_sdk_4.5.0.19786.zip frameworks/flex-config.xml
+mvn org.sonatype.plugins:bundle-publisher-maven-plugin:1.1:deploy-bundle -Dbundle=flex_sdk_4.5.0.19786.zip -Ddescriptor=flex4.5.0.19786_molehill.framework.descriptors.xml -Durl=http://www.flashnexus.org/nexus/content/repositories/snapshots -DrepositoryId=flashnexus
+mvn org.sonatype.plugins:bundle-publisher-maven-plugin:1.1:deploy-bundle -Dbundle=flex_sdk_4.5.0.19786.zip -Ddescriptor=flex4.5.0.19786_molehill.compiler.descriptors.xml -Durl=http://www.flashnexus.org/nexus/content/repositories/snapshots -DrepositoryId=flashnexus
