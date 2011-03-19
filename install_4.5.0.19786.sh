453e86680cf52e100581f36d82b5dbbc61789e49
Conrad Winchester <conrad@mindless.com>
2011-03-19 10:50:00 +0000

    Tidying up standard 4.5.0.19786 install so others can use it

--- install_4.5.0.19786.sh
+++ install_4.5.0.19786.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export MAVEN_OPTS=-Xmx1024m
+
 zip flex_sdk_4.5.0.19786.zip empty.swc
 mvn org.sonatype.plugins:bundle-publisher-maven-plugin:1.1:install-bundle -Dbundle=flex_sdk_4.5.0.19786.zip -Ddescriptor=flex4.5.0.19786.framework.descriptors.xml
 mvn org.sonatype.plugins:bundle-publisher-maven-plugin:1.1:install-bundle -Dbundle=flex_sdk_4.5.0.19786.zip -Ddescriptor=flex4.5.0.19786.compiler.descriptors.xml
