f1b0ae8c1d42f37c0795eeaaed7e100cfc1aed4c
Conrad Winchester <conrad@mindless.com>
2011-05-29 15:56:07 +0100

    Updated to latest incubator and flex SDK release

--- deploy_4.5_molehill.sh
+++ deploy_4.5_molehill.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export MAVEN_OPTS=-Xmx1024m
-zip flex_sdk_4.5.0.19786.zip empty.swc
-zip flex_sdk_4.5.0.19786.zip frameworks/libs/player/11.0/playerglobal.swc
-zip flex_sdk_4.5.0.19786.zip frameworks/flex-config.xml
+zip flex_sdk_4.5.0.20967.zip empty.swc
+zip flex_sdk_4.5.0.20967.zip frameworks/libs/player/11.0/playerglobal.swc
+zip flex_sdk_4.5.0.20967.zip frameworks/flex-config.xml
 
-mvn org.sonatype.plugins:bundle-publisher-maven-plugin:1.1:deploy-bundle -Dbundle=flex_sdk_4.5.0.19786.zip -Ddescriptor=flex4.5.0.19786_molehill.framework.descriptors.xml -Durl=http://www.flashnexus.org/nexus/content/repositories/releases -DrepositoryId=flashnexus
-mvn org.sonatype.plugins:bundle-publisher-maven-plugin:1.1:deploy-bundle -Dbundle=flex_sdk_4.5.0.19786.zip -Ddescriptor=flex4.5.0.19786_molehill.compiler.descriptors.xml -Durl=http://www.flashnexus.org/nexus/content/repositories/releases -DrepositoryId=flashnexus
+mvn org.sonatype.plugins:bundle-publisher-maven-plugin:1.1:deploy-bundle -Dbundle=flex_sdk_4.5.0.20967.zip -Ddescriptor=flex4.5.0.20967_molehill.framework.descriptors.xml -Durl=http://www.flashnexus.org/nexus/content/repositories/releases -DrepositoryId=flashnexus
+mvn org.sonatype.plugins:bundle-publisher-maven-plugin:1.1:deploy-bundle -Dbundle=flex_sdk_4.5.0.20967.zip -Ddescriptor=flex4.5.0.20967_molehill.compiler.descriptors.xml -Durl=http://www.flashnexus.org/nexus/content/repositories/releases -DrepositoryId=flashnexus
